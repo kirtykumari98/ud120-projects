b1993ec0746f7f6f0df4e1694e766911fbb2f37e
kirtykumari98 <62233902+kirtykumari98@users.noreply.github.com>
2020-05-29 11:07:39 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+cls=svm.SVC()
+cls.fit(features_train,labels_train)
+pred=cls.predict(features_train)
 
 #########################################################
 
